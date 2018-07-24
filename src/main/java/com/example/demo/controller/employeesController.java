acc781f609b1986a3c6c021764e1b238f26b5d39
XUHO3 <howells.xu@oocl.com>
2018-07-24 18:46:14 +0800

    add employees and company resource ,and employees http Verb

---
+++ src/main/java/com/example/demo/controller/employeesController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.employees;
+import com.example.demo.service.employeesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class employeesController {
+     @Autowired
+    private employeesService emplService;
+
+
+     @GetMapping("/employees")
+    public  String  getEmployeesList(){
+
+         return null;
+     }
+
+     @GetMapping("/employees/{id}")
+    public String getEmployeesById(@PathVariable int id){
+
+         return  null;
+     }
+
+     @GetMapping("/employees/male")
+    public String getEmployeesByMale(){
+
+         return null;
+    }
+
+    @GetMapping("/employees/page/{page}/pageSize/{pageSize}")
+    public String getEmployeesByPage(@PathVariable int page,@PathVariable int pageSize){
+
+         return null;
+    }
+
+    @PostMapping("/employees")
+    public  String addEmployees(@RequestBody employees empl){
+
+         return  null;
+    }
+
+    @PutMapping("/employees/{id}")
+    public String updateEmployees(@PathVariable int id){
+
+         return  null;
+    }
+
+    @DeleteMapping ("/employees/{id}")
+    public String deleteEmployees(@PathVariable int id){
+
+        return  null;
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/employees.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+public class employees {
+           public company company;
+           public int id ;
+           public String name;
+           public String sex;
+    public com.example.demo.model.company getCompany() {
+        return company;
+    }
+
+    public void setCompany(com.example.demo.model.company company) {
+        this.company = company;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
